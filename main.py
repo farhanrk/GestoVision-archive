e82891e3c4d305af2450a124530c2a3543ea1048
trishir <trishir2m.10@gmail.com>
2023-11-20 16:17:13 -0330

    Added hand landmarks recognition and drawing

--- main.py
+++ main.py
@@ -22,13 +22,28 @@ import mediapipe as mp
 #################################################################################################
 
 def main():
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    # Initialize MediaPipe Hands
+    hands = mp_hands.Hands()
+    # Initialize media capture
     cap = cv2.VideoCapture(1)
     while True:
-        success, img = cap.read()
+        # Get frame
+        success, frame = cap.read()
         if not success:
             break
-        cv2.imshow("Image", img)
-        key_press = cv2.waitKey(30)
+
+        # Process the image and get hand landmarks
+        results = hands.process(frame)
+
+        # Draw landmarks on the image
+        if results.multi_hand_landmarks:
+            for landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
+        cv2.imshow("Image", frame)
+        key_press = cv2.waitKey(1)
         if not key_press == -1:
             break
     cap.release()
