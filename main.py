9dde8748f0f2eb2177c68254cc4d90bfe0383035
trishir <trishir2m.10@gmail.com>
2023-11-20 15:39:59 -0330

    Added live video stream

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 ##  Name                : Farhan Rahman Khan
 ##  Student ID          : 202124798
 #####################################||IMPORTING LIBRARIES||#####################################
-import cv2 as cv
+import cv2
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import simpledialog
@@ -17,30 +17,22 @@ import numpy as np
 import math
 from matplotlib import pyplot as plt
 import mediapipe as mp
+
+
 #################################################################################################
 
 def main():
-    #Using tkinter object to open a file dialogue to browse for the photo 
-    #We'll use openCV ui primarily to display the picture
-    #Creating and hiding a tk object because otherwise the picture window disappears for some reason
-    window = tk.Tk()
-    window.withdraw()
-    img_path = filedialog.askopenfilename(title="Select an Image!") # Opening a dialogue box to browse and open the image we want to work with.
-
-    #Running the program if the selected path to the image is valid
-    if img_path:
-        # Reading and showing the image
-        img = cv.imread(img_path)
-        cv.imshow("The image (press any button to continue)", img)
-        print("Image loaded and shown.")
-        cv.waitKey(0) #waiting for a keey press 
-        cv.destroyAllWindows()
+    cap = cv2.VideoCapture(1)
+    while True:
+        success, img = cap.read()
+        if not success:
+            break
+        cv2.imshow("Image", img)
+        cv2.waitKey(30)
+    cap.release()
+    cv2.destroyAllWindows()
 
 
-    else:
-        print("No image selected.")
-        
-
-#For running the program from terminal
+# For running the program from terminal
 if __name__ == "__main__":
     main()
